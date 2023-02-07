d365b645794c8fe85528044fb04ff09840ad80ce
tomeng70 <tomeng70@users.noreply.github.com>
2023-02-07 12:22:34 -0500

    cleaned up code a little. added definitions for player buttons.

--- EncoderTest/EncoderTest.ino
+++ EncoderTest/EncoderTest.ino
@@ -8,6 +8,12 @@
 #define RED_LED 12
 #define MODE_BUTTON 13
 
+// the button for player 0
+#define BUTTON_0 8
+
+// the button for player 1.
+#define BUTTON_1 7
+
 #define SERVOPIN 9
 
 int counter = 0;
@@ -20,11 +26,17 @@ int seconds = 0;
 int servoPosition = 0;
 Servo clockServo;
 
+// this enumeration is used to represent the mode of the system.
 enum SystemMode {
   MODE_SET,
+  MODE_READY,
   MODE_PLAY
 };
 
+// 0 if player 0, 1 if player 1. 
+unsigned char currPlayer = 0;
+
+// this variable keeps track of the current system mode.
 SystemMode currentMode;
 
 void setup() {
@@ -52,9 +64,10 @@ void loop() {
 
   // refresh LED.
   if (currentMode == MODE_SET) {
-    // turn on LEDs
+    // turn on mode LED.
     digitalWrite(RED_LED, HIGH);
   } else {
+    // turn off mode LED.
     digitalWrite(RED_LED, LOW);
   }
 
@@ -62,47 +75,48 @@ void loop() {
   int modeVal = !digitalRead(MODE_BUTTON);
   if (modeVal == 1) {
     if (currentMode == MODE_SET) {
-      currentMode = MODE_PLAY;
+      currentMode = MODE_READY;
     } else {
       currentMode = MODE_SET;
     }
+    // delay to allow user time to release button.
     delay(250);
   }
 	
   // only update clock through encoders if in MODE_SET.
   if (currentMode == MODE_SET) {
     // Read the current state of CLK
-	currentStateCLK = digitalRead(CLK);
+	  currentStateCLK = digitalRead(CLK);
 
-	// If last and current state of CLK are different, then pulse occurred
-	// React to only 1 state change to avoid double count
-	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+	  // If last and current state of CLK are different, then pulse occurred
+	  // React to only 1 state change to avoid double count
+	  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
 
-		// If the DT state is different than the CLK state then
-		// the encoder is rotating CCW so decrement
-		if (digitalRead(DT) != currentStateCLK) {
-			counter --;
-			currentDir ="CCW";
-		} else {
-			// Encoder is rotating CW so increment
-			counter ++;
-			currentDir ="CW";
-		}
+		  // If the DT state is different than the CLK state then
+		  // the encoder is rotating CCW so decrement
+		  if (digitalRead(DT) != currentStateCLK) {
+		  	counter --;
+			  currentDir ="CCW";
+		  } else {
+			  // Encoder is rotating CW so increment
+			  counter ++;
+			  currentDir ="CW";
+		  }
 
-		Serial.print("Direction: ");
-		Serial.print(currentDir);
-		Serial.print(" | Counter: ");
-		Serial.println(counter);
-	}
+		  Serial.print("Direction: ");
+		  Serial.print(currentDir);
+		  Serial.print(" | Counter: ");
+		  Serial.println(counter);
+	  }
 
-	// Remember last CLK state
-	lastStateCLK = currentStateCLK;
+	  // Remember last CLK state
+	  lastStateCLK = currentStateCLK;
 
-	// Read the button state
-	int btnState = digitalRead(SW);
+  	// Read the button state
+	  int btnState = digitalRead(SW);
 
-	//If we detect LOW signal, button is pressed
-	if (btnState == LOW) {
+	  //If we detect LOW signal, button is pressed
+	  if (btnState == LOW) {
 		//if 50ms have passed since last LOW pulse, it means that the
 		//button has been pressed, released and pressed again
 		if (millis() - lastButtonPress > 50) {
@@ -118,7 +132,6 @@ void loop() {
 
 	// Put in a slight delay to help debounce the reading
 	delay(1);
-
 
   if (counter > 20) {
     counter = 20;
@@ -137,7 +150,5 @@ void loop() {
   clockServo.write(servoPosition);
 
   }
-
 	
-
 }
